badf3c1ac0215099938643ef92b40488927b5d02
amulius <joshua@Joshua-Stortzs-MacBook-Pro.local>
2014-10-24 17:20:42 -0700

    models

--- slides/models.py
+++ slides/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class Person(AbstractUser):
+    is_student = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='profile_image', null=True, blank=True,default='profile_image/default.png')
+
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
+
+class Done(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    student = models.ForeignKey(Person, related_name='is_done')
+
+    def __unicode__(self):
+        return u"{}, {}".format(self.student, self.date)
+
+
+class Help(models.Model):
+    helped = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+    student = models.ForeignKey(Person, related_name='needs_help')
+
+    def __unicode__(self):
+        return u"{}, {}".format(self.student, self.date)
+
+
+class Question(models.Model):
+    answered = models.BooleanField(default=False)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    student = models.ForeignKey(Person, related_name='questions')
+
+    def __unicode__(self):
+        return u"{}, {}".format(self.student, self.date)
+
+
+class Slide(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+
+    def __unicode__(self):
+        return u"{}".format(self.name)

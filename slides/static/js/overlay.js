6710bfa1fb97359cdeb27d3c47b65e21b1bd255c
Jeffcwng <ng.jeffrey@gmail.com>
2014-10-28 14:06:54 -0700

    fixing the h2 and the blue keyline so it works with the action button

--- slides/static/js/overlay.js
+++ slides/static/js/overlay.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
 
+        var hr = '<div class="col-lg-12" id="border"></div>';
+        $('section > h2:first-child').after(hr);
+//    var slideNum = $('.slide-number').text();
+//    console.log(slideNum);
+//    if (slideNum == '0') {
+//        $('#btn_action').hide();
+//        $('#btn_login').hide();
+//    }
+
     // ACTIONS BUTTON
     $('#btn_action').on('click', function() {
     var title = $('section.present').slice(-1).find('h2').text();
         $('#myModalLabel').text(title);
-
     });
 
     // HELP BUTTON
@@ -18,14 +26,15 @@ $(document).ready(function () {
             type: 'POST',
             dataType: 'json',
             data: {
-                'username': username
+                'username': username,
+                'url': url
             },
             success: function(response) {
                 console.log(response);
 
             },
             error: function(response) {
-                console.log(data);
+                console.log(response);
             }
         });
     });
@@ -41,14 +50,15 @@ $(document).ready(function () {
             type: 'POST',
             dataType: 'json',
             data: {
-                'username': username
+                'username': username,
+                'url': url
             },
             success: function(response) {
                 console.log(response);
 
             },
             error: function(response) {
-                console.log(data);
+                console.log(response);
             }
         });
     });
@@ -57,21 +67,24 @@ $(document).ready(function () {
     $('#submit_q').on('click', function() {
     var username = $('#myProfile').data('username');
     var url = window.location.href; // or $(location).attr('href');
-
+    var question = $('#the_question').val();
         console.log(url);
         $.ajax({
             url: '',
             type: 'POST',
             dataType: 'json',
             data: {
-                'username': username
+                'username': username,
+                'url': url,
+                'question': question
             },
             success: function(response) {
+                $('#the_question').val('');
                 console.log(response);
 
             },
             error: function(response) {
-                console.log(data);
+                console.log(response);
             }
         });
     });

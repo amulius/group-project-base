f183a188d85870ed52731bd57407b52e712b84d3
j3dev <j3pydev@gmail.com>
2014-10-28 17:39:46 -0700

    preparing for merge with dev

--- slides/forms.py
+++ slides/forms.py
@@ -13,7 +13,7 @@ class PersonForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta:
         model = Person
-        fields = ("username", "first_name", "last_name", "email", "password1", "password2", "image")
+        fields = ("image", "username", "first_name", "last_name", "email", "password1", "password2")
 
     def clean_username(self):
         # Since User.username is unique, this check is redundant,

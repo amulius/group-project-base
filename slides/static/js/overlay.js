033dd55533d0e81439e81ae2889b2935f2c6a879
j3dev <j3pydev@gmail.com>
2014-10-30 09:05:39 -0700

    removed unnecessary login ajax request

--- slides/static/js/overlay.js
+++ slides/static/js/overlay.js
@@ -43,43 +43,5 @@ $(document).ready(function () {
             }
         });
     });
-
-
-    // LOGIN BUTTON
-    $('#btn_login').on('click', function() {
-    var title = $('section.present').slice(-1).find('h2').text();
-        $('#myModalLabel').text(title);
-    });
-
-
-    $('.login_button').on('click', function() {
-    var url = $(location).attr('register.html');
-    var slide_url = window.location.pathname + window.location.hash;
-    var username = $('#username').val();
-    var password = $('#password').val();
-    var action = $(this).attr('id');
-    var data = {
-                'action': action,
-                'username': username,
-                'register': url,
-                'slide': slide_url
-            };
-
-        console.log(url);
-        $.ajax({
-            url: '/student_actions/',
-            type: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(data),
-            success: function(response) {
-                console.log(response);
-
-            },
-            error: function(response) {
-                console.log(response);
-            }
-        });
-    });
-
 });
 
